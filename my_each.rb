ee17de34b7ae096db32b4852d8577fd8262be8c2
Mark Kanko <mkanko66@gmail.com>
2019-03-19 14:54:42 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,12 @@
 def my_each(words)
+  if block_given?
   i = 0
-  while i < words.length
-    yield(words[i])
-    i += 1
-  end
-  words
+    while i < words.length
+      yield(words[i])
+      i += 1
+    end
+    words
+    else
+    puts "Hey! No block was given!"
+  end 
 end
